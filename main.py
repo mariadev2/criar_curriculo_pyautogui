6325d626ffac68864f44c84eb9317900294ce4e5
mariadev2 <pmariaeduarda987@gmail.com>
2023-01-03 11:28:42 -0300

    iniciando word

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("OLA MUNDO")
+
+import pyautogui as pg
+import time as tm
+import pyperclip
+resposta = str(input("Deseja iniciar um curriculo? (  )sim (  )nao\n"))
+if resposta== "sim":
+    print("vamos lá!")
+    pg.hotkey('win')
+    tm.sleep(3)
+    pg.write('word')
+    pg.press('enter')
+else:
+    print("encerrando...")
